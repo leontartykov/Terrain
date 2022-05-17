ad7175338d56d94617b19fae71ab2e3a36e56752
Lev Tartykov <lev_tartykov@mail.ru>
2022-05-17 15:21:02 +0300

    add: добавлен файл с тестами для карты высот

---
+++ src/ppo_cg_course/unit_tests/test_perlin_noise.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "../core/heightmap/perlin_noise/perlin_original.hpp"
+
+TEST(tests_get_perlin_heightmap_value, positive)
+{
+    int seed = 25;
+    PerlinNoise perlin_noise(seed);
+    double x = 5, y = 7, frequency = 0.5, amplitude = 0.5;
+    double noise_value = perlin_noise.noise2D(x * frequency, y * frequency) * amplitude;
+
+    double result = 0.21875;
+    std::cout << "noise_value: " << noise_value << std::endl;
+    EXPECT_DOUBLE_EQ(result, noise_value);
+}
