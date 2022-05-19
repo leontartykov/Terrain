48e3def0fdb30f1c01cb0160a1972af35673e5a2
Lev Tartykov <lev_tartykov@mail.ru>
2022-05-19 20:20:58 +0300

    add: добавлен класс Point2D

---
+++ src/ppo_cg_course/core/geometry/point/point_2d.h
@@ -0,0 +1,21 @@
+#ifndef _POINT_2D_H_
+#define _POINT_2D_H_
+
+template <typename T>
+class Point2D
+{
+    private:
+        T _x, _y;
+    public:
+        Point2D();
+        Point2D(T x, T y) noexcept;
+        ~Point2D();
+
+        void set_point(const T data_x, const T data_y);
+        T get_x() const;
+        T get_y() const;
+
+        void set_x(T x);
+        void set_y(T y);
+};
+#endif
