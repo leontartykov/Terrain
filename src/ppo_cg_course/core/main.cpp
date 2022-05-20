79fc962f8a0b8cc4eb0fd545cae2290989a87cad
Lev Tartykov <lev_tartykov@mail.ru>
2022-05-20 13:51:04 +0300

    change: сброшен main.cpp

--- src/ppo_cg_course/core/main.cpp
+++ src/ppo_cg_course/core/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include "../core/geometry/point/point_3d.hpp"
 
+#include "object/terrain/terrain.hpp"
 int main()
 {
-    Point3D<int> point_1(1, 1, 2), point_2(2, 2, 3);
-    point_1 += point_2;
-
-    std::cout << point_1.get_x() << std::endl;
+    Terrain terrain;
     return 0;
-
 }
