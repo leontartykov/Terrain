d69f0e5e58168c9a296aef63201867ee5a8af0fb
Lev Tartykov <lev_tartykov@mail.ru>
2022-05-25 01:07:02 +0300

    change: изменен файл main.cpp в соответсвие с классом Entry

--- src/ppo_cg_course/core/main.cpp
+++ src/ppo_cg_course/core/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-#include "object/terrain/terrain.hpp"
+#include "core/entry/entry.h"
+#include "core/entry/app_command/app_command.h"
+#include "core/entry/app_facade/app_facade.h"
+
 int main()
 {
-    Terrain terrain;
+    std::unique_ptr<EntrySystem> system;
+    system = std::unique_ptr<EntrySystem>(new EntrySystem());
+    system->enter_system();
+
     return 0;
 }
